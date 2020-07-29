dd2804337a2a873b2c4cb9a87370862b96000a7a
Jeffrey Gilbert <jgilber@costco.com>
2020-07-28 18:07:49 -0700

    initial import

---
+++ README.md
@@ -0,0 +1,12 @@
+# Hapi Hoster
+
+## Use this Hapi application to host your SPA.
+
+### Put your client files in ./client/dist
+
+### Make sure your running Nodejs v12.16.1 or higher.
+
+### To start the server:
+```
+node server.js
+```
---
+++ server.js
@@ -0,0 +1,66 @@
+'use strict';
+  
+const Hapi = require('@hapi/hapi');
+const Inert = require('@hapi/inert');
+const Path = require('path');
+
+const server = Hapi.server({
+  port: process.env.port || 1234, //default port 1337 is for running on Azure.
+  host: '0.0.0.0',
+  routes: {
+    files: {
+      relativeTo: Path.join(__dirname, './client/dist'),
+    }
+  }
+});
+
+const init = async () => {
+  await server.register([ Inert,
+  {
+    plugin: require('hapi-graceful-shutdown-plugin'),
+    options: {
+      sigtermTimeout: 1,
+      sigintTimeout: 1
+    }
+  }]);
+
+  server.route({
+    method: 'GET',
+    path: '/{path*}',
+    config: {
+      handler: {
+        directory: {
+          path: '.',
+          redirectToSlash: true,
+          listing: false,
+          index: true
+        }
+      }
+    }
+  });
+
+  server.ext('onPreResponse', (request, h) => {
+    const response = request.response;
+    if (response.isBoom &&
+      response.output.statusCode === 404) {
+      return h.file('index.html');
+    }
+    return h.continue;
+  });
+
+  await server.initialize();
+  return server;
+};
+
+const start = async () => {
+  await init();
+  await server.start();
+  console.log(`Server running at: ${server.info.uri}`);
+};
+
+process.on('unhandledRejection', (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+start();
